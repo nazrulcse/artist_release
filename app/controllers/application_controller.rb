1406421f2f2eba8dd8f34604d97ac9ebf4701944
nazrul <nazrulku07@gmail.com>
2016-05-09 12:32:33 +0600

    work with profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name,  :email, :password, :password_confirmation,:last_name)
+    end
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name,  :email, :password, :password_confirmation,:last_name)
+    end
+
+    devise_parameter_sanitizer.for(:sign_in) do |u|
+      u.permit(:email, :password)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,25 @@ class User < ActiveRecord::Base
   belongs_to :sub_category, class_name: 'Category', foreign_key: 'sub_category_id'
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def update_with_password(params, *options)
+    current_password = params.delete(:current_password)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = if params[:password].blank? || valid_password?(current_password)
+               update_attributes(params, *options)
+             else
+               self.assign_attributes(params, *options)
+               self.valid?
+               self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+               false
+             end
+
+    clean_up_passwords
+    result
+  end
 end

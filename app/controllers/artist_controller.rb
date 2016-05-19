5920d1bbc5e3c40cb381455d4a6c13f844a2c085
nazrul <nazrulku07@gmail.com>
2016-05-20 03:18:21 +0600

    implement artist profile and portfolio image

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,5 +1,7 @@
 class ArtistController < ApplicationController
   def index
+    @sub_category = Category.find_by_name(params[:subcategory])
+    @artists = User.where(sub_category_id: @sub_category.id)
   end
 
   def profile
@@ -8,7 +10,7 @@ class ArtistController < ApplicationController
   end
 
   def load_subcategory
-     category =  Category.find_by_id(params[:category_id])
+    category = Category.find_by_id(params[:category_id])
     @subcategories = category.sub_categories
   end
 

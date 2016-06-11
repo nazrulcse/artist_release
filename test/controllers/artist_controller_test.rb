3a037ac50bc52ddc92207eff0c550dc85449710a
nazrul <nazrulku07@gmail.com>
2016-06-11 16:47:59 +0600

    remove test folder

--- test/controllers/artist_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ArtistControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-end
--- test/models/security_question_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SecurityQuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end

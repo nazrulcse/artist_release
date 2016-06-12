1e0c6768145cce53c72ce018df8ffe25dae2ae16
raihan31 <raihan.rubel.31@gmail.com>
2016-06-12 11:39:24 +0600

    application controller test code partially

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
   before (:each) do
-    @user = User.create!(email: 'test@gmail.com', password: '123456789',first_name:'xyz', last_name: 'xyz')
+    @user = User.create!(email: 'test1@gmail.com', password: '123456789',first_name:'xyz', last_name: 'xyz')
+    @user_without_subscription = User.create!(email: 'test2@gmail.com', password: '123456789',first_name:'xyz', last_name: 'xyz')
+    @subscription = Subscription.create!(plan: 'Regular Subscription',amount: "400",user_id:@user.id);
   end
   describe 'ApplicationController' do
     it 'should return resource name' do
@@ -18,6 +20,12 @@ RSpec.describe ApplicationController, type: :controller do
     it 'should return devise mapping' do
       resource = ApplicationController.new
       expect(resource.devise_mapping).to be_an_instance_of(Devise::Mapping)
+    end
+
+    it 'should return after sign in path'do
+      sign_in(@user)
+      application_obj = ApplicationController.new
+      expect(application_obj.after_sign_in_path_for(@user)).to eq('/subscriptions/new')
     end
   end
 end

cc77a8d4172779ada7486e654994965b3be0c58b
nazrul <nazrulku07@gmail.com>
2016-06-15 13:05:17 +0600

    design admin event page and remove unnecessary model from admin

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,17 +1,24 @@
 ActiveAdmin.register Event do
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
+  filter :user
+  filter :title
+  filter :description
+  filter :location
+  filter :created_at
+  index do
+    column :id do |event|
+      link_to event.id, event
+    end
+    column :title
+    column :location do |event|
+      event.location
+    end
+    column :description do |event|
+      raw truncate(event.description, length: 100)
+    end
+    column :date
+    column :image do |event|
+      image_tag(event.image_url, width: 50)
+    end
+    actions
+  end
 end
